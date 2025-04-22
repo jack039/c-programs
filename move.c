62da2694107ad9c80aa6efab3a415424e30232fb
jack039 <133954437+jack039@users.noreply.github.com>
2025-04-22 20:26:31 +0530

    Update move.c

--- move.c
+++ move.c
@@ -3,13 +3,11 @@
 int main() {
     char oldPath[100], newPath[100];
 
-    // Reading the old and new file paths
     printf("Enter old file path: ");
     scanf("%s", oldPath);
     printf("Enter new file path: ");
     scanf("%s", newPath);
 
-    // Renaming (moving) the file
     if (rename(oldPath, newPath) == 0) {
         printf("File moved successfully.\n");
     } else {
